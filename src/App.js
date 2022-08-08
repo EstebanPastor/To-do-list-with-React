6e56d5d5dd20a0b4b6260204ac8d67296eab83b4
EstebanPastor <estebanandrespastor@gmail.com>
2022-08-08 09:59:47 -0300

    To do list finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import { useState } from "react";
+import data from "./data.json";
+import TodoList from "./components/TodoList";
 import "./App.css";
+import TodoForm from "./components/TodoForm";
 
 function App() {
+  const [todos, setTodos] = useState(data);
+
+  const onComplete = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === Number(id)
+          ? { ...todo, completed: !todo.completed }
+          : { ...todo };
+      })
+    );
+
+    todos.map((todo) => {
+      return todo.id === Number(id)
+        ? { ...todo, completed: true }
+        : { ...todo };
+    });
+  };
+
+  const onDeleteItem = (id) => {
+    setTodos([...todos].filter((todo) => todo.id !== id));
+  };
+
+  const addTodo = (newTodo) => {
+    console.log("newTodo", newTodo);
+    let newItem = { id: +new Date(), task: newTodo, completed: false };
+
+    setTodos([...todos, newItem]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      
-      </header>
+    <div className="container">
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        onComplete={onComplete}
+        onDeleteItem={onDeleteItem}
+      />
     </div>
   );
 }
